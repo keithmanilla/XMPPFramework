58d0e07da665ff0c5de41b71e57f9ae9c2f7a70d
Piotr Wegrzynek <pwe@trifork.com>
2017-09-13 15:39:29 +0200

    Add a message storage helper method to process all storage actions for a given stream event in a single batch

--- Extensions/MessageStorage/XMPPMessageCoreDataStorage.h
+++ Extensions/MessageStorage/XMPPMessageCoreDataStorage.h
@@ -1,5 +1,13 @@
 #import "XMPPCoreDataStorage.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
+@class XMPPStream;
+
 @interface XMPPMessageCoreDataStorage : XMPPCoreDataStorage
 
+- (void)scheduleStorageActionForEventWithID:(NSString *)streamEventID inStream:(XMPPStream *)xmppStream withBlock:(dispatch_block_t)block;
+
 @end
+
+NS_ASSUME_NONNULL_END
